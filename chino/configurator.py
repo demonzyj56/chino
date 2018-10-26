a06495b565040231e05f1a9f3b2660aa40737fe8
demonzyj56 <demonzyj56@gmail.com>
2018-10-26 13:25:27 +0800

    Add license notice

--- chino/configurator.py
+++ chino/configurator.py
@@ -1,11 +1,32 @@
+##############################################################################
+# Part of this code is based on:
+#
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+#
+# Based on:
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
 """Build configs, at ease.
 We wish something like the cfg used in Detectron: the default values are stored
 somewhere in config.py and manually specified parameters are parsed and merged
 from a yaml file or command line to the global cfg.
-Notice this module is not thread-safe.
-
-Modified from:
-    https://github.com/facebookresearch/Detectron/blob/master/detectron/core/config.py
 """
 import argparse
 import copy
