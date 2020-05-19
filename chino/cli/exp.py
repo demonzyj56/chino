95ce93ff02f826a87268c60f0b5ab51d8d40dc8f
Yijie Zeng <yijie.zeng@yitu-inc.com>
2020-05-19 15:10:57 +0800

    Add update function and fix some bugs

--- chino/cli/exp.py
+++ chino/cli/exp.py
@@ -1,6 +1,7 @@
 import json
 import os
 import random
+import re
 import shutil
 from string import ascii_lowercase, digits
 from typing import Optional, Dict
@@ -9,7 +10,9 @@ import click
 
 from chino.cli.utils import touch
 from chino.io.fileio import load
-from chino.io.lineio import read_lines
+
+
+PAT = re.compile(r'^E(\d\d)(-.*|$)')  # match Exx or Exx-xxxxx
 
 
 # entry point
@@ -33,6 +36,7 @@ def init(name: str, author: str, email: str) -> None:
     root_folder = os.path.join(os.getcwd(), '.chino-exp')
     if os.path.isdir(root_folder):
         click.echo('Cannot initialize project with name {0}.'.format(name))
+        return
     else:
         try:
             os.makedirs(root_folder)
@@ -41,9 +45,22 @@ def init(name: str, author: str, email: str) -> None:
             return
         info_path = os.path.join(root_folder, 'experiments.json')
         info = dict(name=name, author=author, email=email, exps=[], path=info_path)
-        with open(info_path, 'w') as f:
-            json.dump(info, f)
-        click.echo('Initialized experiment folder for project {0}'.format(name))
+    # catch existing experiments
+    exist_exps = []
+    for folder in os.listdir(os.getcwd()):
+        if not os.path.isdir(folder):
+            continue
+        m = PAT.search(folder)
+        if m is not None:
+            exist_exps.append(m.group(0))
+    if len(exist_exps) > 0 and click.confirm(
+            "Found existing experiments:\n\n{}\n\nAdd to current project?".format('\n'.join(exist_exps))
+    ):
+        for exp_name in exist_exps:
+            info['exps'].append({'name': exp_name, 'desc': None, 'entrypoint': None, 'commit_id': None})
+    with open(info_path, 'w') as f:
+        json.dump(info, f)
+    click.echo('Initialized experiment folder for project {0}'.format(name))
 
 
 @exp.command(name='list')
@@ -73,7 +90,10 @@ def new(annotation: str, desc: str, entrypoint: str) -> None:
     if info is None:
         click.echo('Experiment project folder not initialized or corrupted.')
         return
-    exp_name = 'E{:02d}'.format(len(info['exps'])+1)
+    cur_exp_names = [PAT.search(item['name']) for item in info['exps']]
+    idx = [int(m.group(1)) for m in cur_exp_names if m is not None]
+    max_idx = max(idx) if len(idx) > 0 else 0
+    exp_name = 'E{:02d}'.format(max_idx + 1)
     if len(annotation) > 0:
         exp_name += '-{}'.format(annotation.replace(' ', '-'))
     entry_file = os.path.join(os.getcwd(), exp_name, entrypoint)
@@ -84,10 +104,40 @@ def new(annotation: str, desc: str, entrypoint: str) -> None:
           record_create_datetime=True,
           create_dir=True,
           executable=True)
-    info['exps'].append({'name': exp_name, 'desc': desc, 'entrypoint': entry_file})
+    info['exps'].append({'name': exp_name, 'desc': desc, 'entrypoint': entry_file, 'commit_id': None})
     with open(info['path'], 'w') as f:
         json.dump(info, f)
     click.echo('Initialized experiment {0}'.format(exp_name))
+
+
+@exp.command()
+@click.option('--name', '-n', type=str, prompt=True,
+              help='Name of the experiment.')
+@click.option('--commit_id', '-c', type=str, default=None)
+def update(name: str, commit_id: str) -> None:
+    """Update information of one experiment. The exp_name should match at least
+    one experiment. Currently supported information includes commit_id."""
+    # first match the name
+    info = get_project_info()
+    if info is None:
+        click.echo('Experiment project folder not initialized or corrupted.')
+        return
+    exp_name = None
+    for e in info['exps']:
+        if re.search(name, e['name']) is not None:
+            exp_name = e['name']
+            exp_info = e
+            break
+    if exp_name is None:
+        click.echo('Unable to find any experiment matching {0}. Exiting.'.format(name))
+        return
+    if commit_id is None:
+        commit_id = click.prompt('commit_id')
+    old_id = exp_info.get('commit_id', None)
+    exp_info['commit_id'] = commit_id
+    with open(info['path'], 'w') as f:
+        json.dump(info, f)
+    click.echo('Updated commit_id {} -> {}.'.format(old_id, commit_id))
 
 
 @exp.command()
